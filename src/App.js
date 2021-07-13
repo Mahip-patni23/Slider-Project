a92e4a82d9102eb4eb149f13cba04b1b15a7f77d
Your Name <you@example.com>
2021-07-13 12:00:39 -0700

    Slider Project
    A simple slider project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import { FaQuoteRight } from 'react-icons/fa';
+import data from './data';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+  const people = data;
+  const [index, setIndex] = useState(0);
+
+  /* const { id, image, name, quote, title } = people[index]; */
+  useEffect(() => {
+    if(index<0){
+      setIndex(people.length-1)
+    }else if(index >= people.length)
+    {
+      setIndex(0);
+    }
+  }, [index, people])
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex(index+1);
+    }, 3000)
+    return () => clearInterval(interval);
+  }, [index])
+
+  return (<>
+    <main>
+      <header className="heading">
+        <h2>reviews <span>.</span></h2>
       </header>
-    </div>
+      <section className="section-center">
+        {
+          people.map((person, personIndex) => {
+            const {id, name, image, title, quote } = person;
+            let position = 'nextslide';
+            if(personIndex===index)
+            {
+              position = 'activeslide';
+            }
+            if(personIndex === index-1 || (index===0 && personIndex===people.length-1))
+            {
+              position = 'lastslide';
+            }
+            return <article key={id} className={position}>
+              <img src={image} alt={name} className="person-img" />
+              <h4>{name}</h4>
+              <p className="title">{title}</p>
+              <p className="text">{quote}</p>
+              <FaQuoteRight className="icon"></FaQuoteRight>
+            </article>
+          })
+        }
+
+        {/* <article key="id" className="single-review">
+          <img src={image} alt={name} className="person-img" />
+          <h4>{name}</h4>
+          <p className="title">{title}</p>
+          <p className="text">{quote}</p>
+          <FaQuoteRight className="icon"></FaQuoteRight>
+        </article> */}
+        <button className="prev btn" onClick={() => setIndex(index-1)}>
+          <FiChevronLeft></FiChevronLeft>
+        </button>
+        <button className="next btn" onClick={() => setIndex(index+1)}>
+          <FiChevronRight></FiChevronRight>
+        </button>
+      </section>
+    </main>
+    <footer>
+      <p>copyright @ <span>mahip-patni23...</span></p>
+    </footer>
+    </>
+    
   );
 }
 
